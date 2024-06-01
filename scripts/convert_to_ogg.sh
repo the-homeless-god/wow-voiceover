f2f09cc365defd3e9996edfac95aef67da59288c
Marat Zimnurov <zimtir@mail.ru>
2024-06-01 13:01:01 +0300

    ci(scripts): extend packer

--- scripts/convert_to_ogg.sh
+++ scripts/convert_to_ogg.sh
@@ -1,2 +1,5 @@
 for f in *.mp3; do mv -- "$f" "${f%.mp3}.wav"; done
-for f in *.wav; do ffmpeg -i "$f" -y -n -c:a libvorbis -vbr on -compression_level 10 -frame_duration 60 -application voip "${f%.*}.ogg"; done
+for f in *.mp3; do ffmpeg -i "$f" -n -c:a libvorbis -q 0 -vbr on -compression_level 10 -frame_duration 60 -application voip "${f%.*}.ogg"; done
+
+mv ./AI_VoiceOverData_Vanilla/generated/sounds/quests/*.ogg AI_VoiceOverData_Vanilla_OGG/generated/sounds/quests
+mv ./AI_VoiceOverData_Vanilla/generated/sounds/gossip/*.ogg AI_VoiceOverData_Vanilla_OGG/generated/sounds/gossip
